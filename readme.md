6083f57c037d56f16b8805a2be4f6ca5363a5065
Guillaume DEREX <gderex8@orange.fr>
2022-09-05 22:00:31 +0200

    Made template

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Zig SFML Template
+
+## Template for setting up an SFML project with Zig fast
+
+Uses Guigui220D's [zig-sfml-wrapper](https://github.com/Guigui220D/zig-sfml-wrapper).
+
+Clone with submodules. Make sure CSFML is installed. Run with `zig build run`.
---
+++ src/main.zig
@@ -0,0 +1,24 @@
+//! This is a translation of the c++ code the sfml website gives you to test if SFML works
+//! for instance, in this page: https://www.sfml-dev.org/tutorials/2.5/start-vc.php
+
+const sf = @import("sfml").graphics;
+
+pub fn main() !void {
+    var window = try sf.RenderWindow.createDefault(.{ .x = 200, .y = 200 }, "SFML works!");
+    defer window.destroy();
+
+    var shape = try sf.CircleShape.create(100.0);
+    defer shape.destroy();
+    shape.setFillColor(sf.Color.Green);
+
+    while (window.isOpen()) {
+        while (window.pollEvent()) |event| {
+            if (event == .closed)
+                window.close();
+        }
+
+        window.clear(sf.Color.Black);
+        window.draw(shape, null);
+        window.display();
+    }
+}
